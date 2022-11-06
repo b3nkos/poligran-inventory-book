e17ca693b91599a4d4a25c58d670c7c348474de7
Cristian Martinez <cristianandres92@gmail.com>
2022-11-06 16:44:20 -0500

    cors problems fix

--- backend/src/main/java/co/edu/poli/acmelibrary/BookInventoryApplication.java
+++ backend/src/main/java/co/edu/poli/acmelibrary/BookInventoryApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -14,6 +16,16 @@ public class BookInventoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookInventoryApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
